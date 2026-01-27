378e2eeaf5b4a51f62f4d514ffc70d2bee17c7aa
dcmcshan <dan@syzygyx.com>
2026-01-27 12:25:46 -0700

    Move to InquiryInstitute org - update all URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dcmcshan.github.io',
+  site: 'https://inquiryinstitute.github.io',
   base: '/course-mhh-voight-kampff',
   integrations: [tailwind()],
   output: 'static',
